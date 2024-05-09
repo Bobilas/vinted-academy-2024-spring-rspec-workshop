4a4c45ccc762303d9101a9f7c035ac08146f48e0
Povilas Sutkaitis <povilas.sutkaitis@vinted.com>
2024-05-09 10:59:54 +0300

    solve 03

--- spec/domain/items/nsfw_validator_spec.rb
+++ spec/domain/items/nsfw_validator_spec.rb
@@ -4,42 +4,66 @@ RSpec.describe Items::NsfwValidator do
   let(:validator) { described_class.new(item_name) }
 
   describe '#nsfw?' do
-    # TODO: use subject and convert example groups to one-liners
+    subject { validator.nsfw? }
 
     context 'when nsfw lowercase' do
       let(:item_name) { 'balls' }
 
-      it 'returns true' do
-        expect(validator.nsfw?).to be true
+      it { is_expected.to be true }
+    end
+
+    context 'when nsfw uppercase' do
+      let(:item_name) { 'Radioactive Balls' }
+
+      it { is_expected.to be true }
+    end
+
+    context 'when sfw' do
+      let(:item_name) { 'This is completely safe' }
+
+      it { is_expected.to be false }
+    end
+
+    context 'when nil' do
+      let(:item_name) { nil }
+
+      it { is_expected.to be false }
+    end
+  end
+
+  describe '#validate' do
+    subject { validator.validate }
+
+    context 'when nsfw lowercase' do
+      let(:item_name) { 'balls' }
+
+      it 'raises error' do
+        expect { subject }.to raise_error(StandardError).with_message('item is nsfw')
       end
     end
 
     context 'when nsfw uppercase' do
       let(:item_name) { 'Radioactive Balls' }
 
-      it 'returns true' do
-        expect(validator.nsfw?).to be true
+      it 'raises error' do
+        expect { subject }.to raise_error(StandardError).with_message('item is nsfw')
       end
     end
 
     context 'when sfw' do
       let(:item_name) { 'This is completely safe' }
 
-      it 'returns false' do
-        expect(validator.nsfw?).to be false
+      it 'raises error' do
+        expect { subject }.not_to raise_error
       end
     end
 
     context 'when nil' do
       let(:item_name) { nil }
 
-      it 'returns false' do
-        expect(validator.nsfw?).to be false
+      it 'raises error' do
+        expect { subject }.not_to raise_error
       end
     end
-  end
-
-  describe '#validate' do
-    # TODO: use subject and verify if error is raised or not
   end
 end
