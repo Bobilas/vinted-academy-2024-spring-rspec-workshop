2c402b5aac2d6d90fa31e1e18e4d9bb208106d3d
Povilas Sutkaitis <povilas.sutkaitis@vinted.com>
2024-05-09 11:40:40 +0300

    solve 04

--- spec/domain/users/facade_spec.rb
+++ spec/domain/users/facade_spec.rb
@@ -2,14 +2,66 @@
 
 RSpec.describe Users::Facade do
   describe '.register' do
-    # TODO: write tests
+    subject { described_class.register(name) }
+
+    let(:name) { 'test_user' }
+
+    it 'registers new user' do
+      expect { subject }.to change(User, :count).by(1)
+      expect(User).to exist(name: name)
+    end
+
+    context 'when name is nil' do
+      let(:name) { nil }
+
+      it 'fails' do
+        expect { subject }.to raise_error ActiveRecord::NotNullViolation
+      end
+    end
+
+    context 'with duplicate name' do
+      let!(:user) { create :user, name: name }
+
+      it 'fails' do
+        expect { subject }.to raise_error ActiveRecord::RecordNotUnique
+      end
+    end
   end
 
   describe '.verify' do
-    # TODO: write tests
+    subject { described_class.verify(user_id) }
+
+    let(:user_id) { user.id }
+    let(:user) { create :user }
+
+    it 'verifies user' do
+      expect { subject }.to change { user.reload.is_verified }.from(false).to(true)
+    end
+
+    context 'when user is not found' do
+      let(:user_id) { -1 }
+
+      it 'fails' do
+        expect { subject }.to raise_error(StandardError).with_message('not found')
+      end
+    end
+
+    context 'when user is already verified' do
+      let(:user) { create :user, :verified }
+
+      it 'fails' do
+        expect { subject }.to raise_error(StandardError).with_message('already verified')
+      end
+    end
   end
 
   describe '.verified_user_ids' do
-    # TODO: write tests
+    subject { described_class.verified_user_ids }
+
+    let!(:verified_users) { create_list :user, 2, :verified }
+    let!(:unverified_users) { create_list :user, 2 }
+    let(:verified_user_ids) { verified_users.pluck(:id) }
+
+    it { is_expected.to match_array verified_user_ids }
   end
 end
