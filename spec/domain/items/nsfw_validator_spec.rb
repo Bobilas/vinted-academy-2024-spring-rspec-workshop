90feb365c1a5857c4063c108b658031e3e8912e9
Povilas Sutkaitis <povilas.sutkaitis@vinted.com>
2024-05-09 13:57:31 +0300

    fix description

--- spec/domain/items/nsfw_validator_spec.rb
+++ spec/domain/items/nsfw_validator_spec.rb
@@ -10,14 +10,18 @@ RSpec.describe Items::NsfwValidator do
       # TODO: write test
     end
 
-    it 'BONUS 1: fix this failing test and change example group description' do
+    it 'BONUS 1: fix this failing test and change example description' do
       validator = described_class.new('Radioactive Balls')
       expect(validator.nsfw?).to be true
     end
 
-    it 'BONUS 2: fix this failing test and change example group description' do
+    it 'BONUS 2: fix this failing test and change example description' do
       validator = described_class.new(nil)
       expect(validator.nsfw?).to be false
     end
+  end
+
+  it 'fails' do
+    expect { 1 / 0 }.to raise_error(ZeroDivisionError)
   end
 end
