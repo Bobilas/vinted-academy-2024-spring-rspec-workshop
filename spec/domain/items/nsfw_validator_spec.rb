3bf1e5108b69dee733ad8044058d6cf5ebd4bff1
Povilas Sutkaitis <povilas.sutkaitis@vinted.com>
2024-05-09 10:52:19 +0300

    solve 02, prepare 03

--- spec/domain/items/nsfw_validator_spec.rb
+++ spec/domain/items/nsfw_validator_spec.rb
@@ -1,39 +1,45 @@
 # frozen_string_literal: true
 
 RSpec.describe Items::NsfwValidator do
+  let(:validator) { described_class.new(item_name) }
+
   describe '#nsfw?' do
-    # TODO: extract `validator` and `item_name` using let
+    # TODO: use subject and convert example groups to one-liners
 
     context 'when nsfw lowercase' do
+      let(:item_name) { 'balls' }
+
       it 'returns true' do
-        item_name = 'balls'
-        validator = described_class.new(item_name)
         expect(validator.nsfw?).to be true
       end
     end
 
     context 'when nsfw uppercase' do
+      let(:item_name) { 'Radioactive Balls' }
+
       it 'returns true' do
-        item_name = 'Radioactive Balls'
-        validator = described_class.new(item_name)
         expect(validator.nsfw?).to be true
       end
     end
 
     context 'when sfw' do
+      let(:item_name) { 'This is completely safe' }
+
       it 'returns false' do
-        item_name = 'This is completely safe'
-        validator = described_class.new(item_name)
         expect(validator.nsfw?).to be false
       end
     end
 
     context 'when nil' do
+      let(:item_name) { nil }
+
       it 'returns false' do
-        item_name = nil
-        validator = described_class.new(item_name)
         expect(validator.nsfw?).to be false
       end
     end
+  end
+
+  describe '#validate' do
+    # TODO: use subject and verify if error is raised or not
   end
 end
